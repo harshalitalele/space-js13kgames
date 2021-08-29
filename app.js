933e79249b6d3c2c86da0948f785357bef332396
Harshali Talele <harshalitalele@gmail.com>
2021-08-29 12:59:54 -0700

    background changes

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
-function showAlert() {
-	alert("Hey there!");
+var can = document.getElementById('background-canvas');
+var ctx = can.getContext('2d');
+  
+// canvas width and height
+can.width = window.window.innerWidth;
+can.height = window.window.innerHeight;
+var img = new Image();
+img.src = "background.jpg";
+
+window.onload = function() {
+    var imgHeight = 0;
+    var scrollSpeed = 3;
+    function loop()
+    {
+        ctx.drawImage(img, 0, imgHeight);
+        ctx.drawImage(img, 0, imgHeight - can.height);
+        imgHeight += scrollSpeed;
+        if (imgHeight == can.height)
+            imgHeight = 0;
+        window.requestAnimationFrame(loop);
+    }
+    loop();
 }
